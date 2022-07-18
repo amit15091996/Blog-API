099c25a651cacf2877e2a259a19284a52ec5087a
amit15091996 <amitdewangan199@gmail.com>
2022-07-18 15:32:45 +0530

    changes

--- src/test/java/com/blog/BlogApiApplicationTests.java
+++ src/test/java/com/blog/BlogApiApplicationTests.java
@@ -3,27 +3,13 @@ package com.blog;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.blog.repo.UserRepo;
 
 @SpringBootTest
 class BlogApiApplicationTests {
 
-	@Autowired
-	private UserRepo repo;
-
-
 	@Test
 	 void repoTest() {
-
-		String className = this.repo.getClass().getName();
-		Package packageName = this.repo.getClass().getPackage();
-		this.repo.getClass().getPackage();
-		System.out.println(className);
-		System.out.println(packageName);
-
 		String str="";
 		assertEquals("",str);
 	}
